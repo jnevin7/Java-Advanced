b8315486d322a5de276073a8b8c1f3ba680c050b
jnevin7 <144211993+jnevin7@users.noreply.github.com>
2025-11-19 18:14:55 +0200

    Delete Director.java

--- Director.java
+++
@@ -1,8 +0,0 @@
-package ex03_1_exercise;
-
-public class Director extends Manager{
-    Director(String name, int age, double salary,String deptName) {
-        super(name, age, salary,deptName);
-    }
-
-}
