f477b7627657351f0fbce51acba1ce54497546c9
jnevin7 <144211993+jnevin7@users.noreply.github.com>
2026-05-07 16:25:14 +0200

    Add files via upload

---
+++ Chapter 6/Cat.java
@@ -0,0 +1,35 @@
+package ex06_1_exercise;
+
+public class Cat extends Animal implements Pet{
+
+    public String name;
+
+    public Cat(String name) {
+        super(4);
+        this.name = name;
+    }
+
+    public Cat() {
+        this("Fluffy");
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void play() {
+        System.out.println(name + " likes to play with string.");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("Cats like to eat spiders and fish.");
+    }
+}
---
+++ Chapter 6/Pet.java
@@ -0,0 +1,7 @@
+package ex06_1_exercise;
+
+public interface Pet {
+    public String getName();
+    public void setName(String name);
+    public void play();
+}
