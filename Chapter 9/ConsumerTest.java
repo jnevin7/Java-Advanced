e90a79e593798b1308128a6199e6280d2b4fc1de
jnevin7 <144211993+jnevin7@users.noreply.github.com>
2026-05-07 16:27:37 +0200

    Add files via upload

---
+++ Chapter 9/ConsumerTest.java
@@ -0,0 +1,22 @@
+package ex09_1_exercise;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+public class ConsumerTest {
+  
+  public static void main(String[] args) {
+    
+    List<Employee> eList = Employee.createShortList();
+    Employee first = eList.get(0);
+    
+    Consumer<Employee> eCons = e -> System.out.println(
+            "Name: " + e.getSurName() + "\nRole: "
+            + e.getRole() + "\nSalary: " + e.getSalary()
+    ); // Write your consumer lambda here
+    
+    System.out.println("=== First Salary");
+    eCons.accept(first);
+    
+  }
+}
---
+++ Chapter 9/Role.java
@@ -0,0 +1,3 @@
+package ex09_1_exercise;
+
+public enum Role { STAFF, MANAGER, EXECUTIVE }
